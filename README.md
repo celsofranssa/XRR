f8acbccdecf5eff7a3fb8067cf133a75e3acab4e
Celso <c.el@live.com>
2022-05-11 16:26:54 -0300

    update: fix settings.yaml

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# lightning_prototype
+# Lightning Prototype
+
+### 1. Quick Start
+
+```shell script
+# clone the project 
+git clone git@github.com:celsofranssa/LightningPrototype.git
+
+# change directory to project folder
+cd LightningPrototype/
+
+# Create a new virtual environment by choosing a Python interpreter 
+# and making a ./venv directory to hold it:
+virtualenv -p python3 LightningPrototype/
+
+# activate the virtual environment using a shell-specific command:
+source ./LightningPrototype/bin/activate
+
+# install dependecies
+pip install -r requirements.txt
+
+# setting python path
+export PYTHONPATH=$PATHONPATH:<path-to-project-dir>/LightningPrototype/
+
+# (if you need) to exit virtualenv later:
+deactivate
+```
--- source/model/LitAutoEncoder.py
+++ source/model/LitAutoEncoder.py
@@ -9,9 +9,24 @@ class LitAutoEncoder(pl.LightningModule):
     def __init__(self, hparams):
         super().__init__()
         self.save_hyperparameters(hparams)
-        self.encoder = nn.Sequential(nn.Linear(28 * 28, 128), nn.ReLU(), nn.Linear(128, 3))
-        self.decoder = nn.Sequential(nn.Linear(3, 128), nn.ReLU(), nn.Linear(128, 28 * 28))
+
+        # encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(self.hparams.encoder.width * self.hparams.encoder.height, self.hparams.hidden_size),
+            nn.ReLU(),
+            nn.Linear(self.hparams.hidden_size, self.hparams.encoder.output_size)
+        )
+
+        # decoder
+        self.decoder = nn.Sequential(
+            nn.Linear(self.hparams.decoder.input_size, self.hparams.hidden_size),
+            nn.ReLU(),
+            nn.Linear(self.hparams.hidden_size, self.hparams.decoder.width * self.hparams.decoder.height)
+        )
+
+        # loss
         self.loss = nn.MSELoss()
+
 
     def training_step(self, batch, batch_idx):
         x, y = batch["x"], batch["y"]
@@ -38,5 +53,12 @@ class LitAutoEncoder(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        # optimizer
+        optimizer = torch.optim.AdamW(
+            self.parameters(),
+            lr=self.hparams.lr,
+            betas=(0.9, 0.999),
+            eps=1e-08,
+            weight_decay=self.hparams.weight_decay,
+            amsgrad=True)
         return optimizer
